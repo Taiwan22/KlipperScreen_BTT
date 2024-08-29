4458592ac98155fd4102e464131b161b0655cc83
Taiwan22 <17104157+Taiwan22@users.noreply.github.com>
2024-08-29 21:31:21 +0200

    fix4

--- ks_includes/printer.py
+++ ks_includes/printer.py
@@ -166,9 +166,11 @@ class Printer:
         self.moon_sensors = {}
 
         logging.debug(f"Processing moonraker sensors: {data}")
-        for x in data['sensors'].values():
-            self.moon_sensors[x['id']] = x['values']
-        logging.debug(f"Moonraker sensors: {self.moon_sensors}")
+        if "sensors" in data:
+            for x in data['sensors'].values():
+                if "values" in x and "id" in x:
+                    self.moon_sensors[x['id']] = x['values']
+            logging.debug(f"Moonraker sensors: {self.moon_sensors}")
 
     def get_config_section_list(self, search=""):
         if self.config is not None:
